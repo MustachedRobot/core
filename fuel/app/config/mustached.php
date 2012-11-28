2a747dfd8a371fc2ae315037b28873b8cf398dfd
Dzey <jeremie.pottier@gmail.com>
2012-11-28 06:33:21 +0100

    styling the geolocation

--- fuel/app/config/mustached.php
+++ fuel/app/config/mustached.php
@@ -4,7 +4,7 @@ return array(
 	'salt'  => 'iUèl;=$_Iulnfd335j,U',
 	'seats' => 12,
 	'geolocation' => array(
-		'active'  => true,
+		'active'  => false,
 		'latitude'  => '47.21174260',
 		'longitude' => '-1.55152980',
 		'accuracy'  => '0.400', // If the user is distant from more than this number (in km), he won't be able to checkin
